d3dbec136c9126c63a28822bbd59f271213e43ec
Sebastian Blasetti <sblasetti@gmail.com>
2020-07-15 12:52:08 -0300

    Episode 9: implement reloader to be added to the weapons that need it

---
+++ Assets/Scripts/Combat/WeaponReloader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponReloader : MonoBehaviour
+{
+    [SerializeField]
+    int maxAmmo;
+    [SerializeField]
+    float reloadTime;
+    [SerializeField]
+    int clipSize;
+
+    int ammo;
+    int shotsFiredInClip;
+    bool isReloading;
+
+    int shotsRemainingInClip => clipSize - shotsFiredInClip;
+    //public bool IsReloading => isReloading;
+
+    public void Reload()
+    {
+        if (isReloading) return;
+
+        isReloading = true;
+        GameManager.GetInstance().GetTimer().Add(reloadTime, ExecuteReload);
+    }
+
+    public void ExecuteReload()
+    {
+        isReloading = false;
+        ammo -= shotsFiredInClip;
+        shotsFiredInClip = 0;
+
+        // TODO: why could ammo be negative?
+        if (ammo < 0)
+        {
+            shotsFiredInClip -= ammo;
+            ammo = 0;
+        }
+    }
+}
