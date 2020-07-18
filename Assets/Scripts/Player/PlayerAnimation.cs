287cdad9246d014f7880e041893987715b0b2d8d
Sebastian Blasetti <sblasetti@gmail.com>
2020-07-18 00:22:52 -0300

    Episode 11: run animation

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    Animator animator;
+    InputController inputController;
+
+    private void Awake()
+    {
+        inputController = GameManager.GetInstance().GetInputController();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetFloat("Vertical", inputController.Vertical);
+        animator.SetFloat("Horizontal", inputController.Horizontal);
+    }
+}
