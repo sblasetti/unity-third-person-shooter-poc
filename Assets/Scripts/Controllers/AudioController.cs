94ac622123f85ffb5fac8f11c2cc0cce1d14be6a
Sebastian Blasetti <sblasetti@gmail.com>
2020-08-11 01:21:02 -0300

    Episode 18: audio controller

---
+++ Assets/Scripts/Controllers/AudioController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] AudioClip[] clips;
+    [SerializeField] float delayBetweenClips;
+
+    bool canPlay;
+    private AudioSource source;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void Play()
+    {
+        if (!canPlay) return;
+
+        GameManager.GetInstance().GetTimer().Add(delayBetweenClips, () => {
+            canPlay = true;
+        });
+
+        canPlay = false;
+        var random = UnityEngine.Random.Range(0, clips.Length);
+        source.PlayOneShot(clips[random]);
+    }
+}
